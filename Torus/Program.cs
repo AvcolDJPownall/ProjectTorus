83a273ac1bd7bc655fc4906d74b3a332173a1e68
DJ Pownall <ac107151@avcol.school.nz>
2022-06-29 12:28:11 +1200

    Implement basic sign-in/user identity functionality

--- Torus/Program.cs
+++ Torus/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Torus.Areas.Identity.Data;
+using Torus.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TorusContextConnection") ?? throw new InvalidOperationException("Connection string 'TorusContextConnection' not found.");
+
+builder.Services.AddDbContext<TorusContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<TorusUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<TorusContext>();;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,9 +28,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
