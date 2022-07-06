4001f6d288a541e6ac7879cd14f38f0631ba6014
DJ Pownall <ac107151@avcol.school.nz>
2022-07-07 11:04:08 +1200

    Use shared enum for asset types

--- Torus/Areas/Identity/Data/TorusUser.cs
+++ Torus/Areas/Identity/Data/TorusUser.cs
@@ -3,28 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Torus.Models;
 
 namespace Torus.Areas.Identity.Data;
 
 
-// Bitmask representing a user's prefered area(s) of 3D art.
-// Can represent any combination of artist types using bitwise OR operations.
-public enum ArtistType
-{
-    None = 0,
-    Modelling = 1,
-    Shader = 2,
-    Particles = 4,
-    NormalMap = 8,
-    TextureDesigner = 16,
-    Animation = 32,
-    Rigging = 64,
-};
-
 public class TorusUser : IdentityUser
 {
-    public ArtistType ArtistType { get; set; } = 0;
+    public AssetType ArtistType { get; set; } = 0;
     public long Balance { get; set; }
-    public string Bio = "";
+    public string? Bio { get; set; }
 }
 
