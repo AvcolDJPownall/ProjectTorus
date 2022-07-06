c571985a370c962a9ff8fdea0ad2ad43f77b6dc8
DJ Pownall <ac107151@avcol.school.nz>
2022-07-06 12:15:23 +1200

    Add account balance + artist preferences

--- Torus/Areas/Identity/Data/TorusUser.cs
+++ Torus/Areas/Identity/Data/TorusUser.cs
@@ -6,8 +6,25 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Torus.Areas.Identity.Data;
 
-// Add profile data for application users by adding properties to the TorusUser class
+
+// Bitmask representing a user's prefered area(s) of 3D art.
+// Can represent any combination of artist types using bitwise OR operations.
+public enum ArtistType
+{
+    None = 0,
+    Modelling = 1,
+    Shader = 2,
+    Particles = 4,
+    NormalMap = 8,
+    TextureDesigner = 16,
+    Animation = 32,
+    Rigging = 64,
+};
+
 public class TorusUser : IdentityUser
 {
+    public ArtistType ArtistType { get; set; } = 0;
+    public long Balance { get; set; }
+    public string Bio = "";
 }
 
